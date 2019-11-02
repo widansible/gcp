a75442e590229626035d5b4f917dbf615d72192a
widyamedia <widyamedia@gmail.com>
2019-11-03 06:59:02 +0700

    start

--- 21-gcp-start-stop.sh
+++ 21-gcp-start-stop.sh
@@ -3,17 +3,21 @@
 # Constants - CHANGE ME!
 readonly PROJECT='ansible-257005'
 readonly ZONE='asia-southeast1-a'
+NAMA=$1
+ACT=$2
 
 if [ -z "$1" ]; then
- NAMA=$1
  while [ -z "$NAMA" ]; do
   echo -n "Masukan nama vm: "
   read NAMA
  done
- 
- echo -e "Action:\n\n- start\n- stop\n"
- echo -n "Pilih Action yang akan dilakukan: "
- read ACT
- 
+ else
+ if [ -z "$2" ]; then
+  while [ -z "$ACT" ]; do
+   echo -e "Action:\n\n- start\n- stop\n"
+   echo -n "Pilih Action yang akan dilakukan: "
+   read ACT
+  done
+ fi
  time yes | gcloud compute instances $ACT $NAMA --zone=$ZONE
 fi
